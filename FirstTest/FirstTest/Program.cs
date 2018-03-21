9785a457f78cf1b55831eb48e5611d4eba1c416a
wamri <wat777wat@hotmail.com>
2018-03-22 02:17:28 +0300

    New Day

---
+++ FirstTest/FirstTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 10;
+            for (int i = 0; i < max; i++)
+            {
+                Console.WriteLine("Hello World!"+i);
+
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
